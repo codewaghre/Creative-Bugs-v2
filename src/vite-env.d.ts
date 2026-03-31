f5d86eef6debc889cb85ed1708ce2d0c653b6830
Waghre Abhishek <waghreabhishek@gmail.com>
2026-03-31 23:43:31 +0530

    vite env ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,26 +1,7 @@
 /// <reference types="vite/client" />
 
-declare module "*.png" {
-  const src: string;
-  export default src;
-}
-
-declare module "*.jpg" {
-  const src: string;
-  export default src;
-}
-
-declare module "*.jpeg" {
-  const src: string;
-  export default src;
-}
-
-declare module "*.svg" {
-  const src: string;
-  export default src;
-}
-
-declare module "*.mp4" {
-  const src: string;
-  export default src;
-}
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.svg";
+declare module "*.jpeg";
+declare module "*.mp4";
