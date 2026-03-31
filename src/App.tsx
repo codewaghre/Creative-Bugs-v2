c01dd065c92899475e97365b72f641148eec2bf9
Waghre Abhishek <waghreabhishek@gmail.com>
2026-03-31 23:56:55 +0530

    lenis update

--- src/App.tsx
+++ src/App.tsx
@@ -3,39 +3,47 @@ import { About, Home, Work } from "@pages/index"
 import { MainLayout } from "@common/index"
 import { AskQuestionForm } from "./components/Common/AskQuestionForm"
 import { BookAMeetingFrom } from "./components/Common/BookAMeetingFrom"
-import ReactLenis from "lenis/react"
+import Lenis from "lenis"
+import { useEffect } from "react"
 
 
 
 function App() {
 
+
+  useEffect(() => {
+    const lenis = new Lenis({
+      lerp: 0.1,
+      duration: 1.2,
+      smoothWheel: true,
+    })
+
+    function raf(time: number) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf)
+
+    return () => {
+      lenis.destroy()
+    }
+  }, [])
+
   return (
     <>
-      <ReactLenis
-        root
-        options={{
-          lerp: 0.1,
-          duration: 1.2,
-          orientation: "vertical",
-          gestureOrientation: "vertical",
-          smoothWheel: true,
-          wheelMultiplier: 1,
-          touchMultiplier: 2
-        }}
-      >
 
 
-        <Routes>
-          <Route element={<MainLayout />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/work" element={<Work />} />
-            <Route path="/contactform" element={<AskQuestionForm />} />
-            <Route path="/bookameetingform" element={<BookAMeetingFrom />} />
-          </Route>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/work" element={<Work />} />
+          <Route path="/contactform" element={<AskQuestionForm />} />
+          <Route path="/bookameetingform" element={<BookAMeetingFrom />} />
+        </Route>
 
-        </Routes>
-      </ReactLenis>
+      </Routes>
     </>
   )
 }
