6d5884e9ee75df2241eaf8bfe8c964c7e7c54d91
Waghre Abhishek <waghreabhishek@gmail.com>
2026-04-27 17:41:48 +0530

    remove theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,18 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
-import { ThemeProvider } from './app/themeProvider.tsx'
+
 import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
 
-      <ThemeProvider>
-        <Toaster position="bottom-center"
-          reverseOrder={false} />
-        <App />
-      </ThemeProvider>
+      <Toaster position="bottom-center"
+        reverseOrder={false} />
+      <App />
+
     </BrowserRouter>
   </StrictMode>,
 )
