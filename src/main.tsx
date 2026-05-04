c2ad6c6bcba688b86f13ad26f662b5dd5807eedb
Waghre Abhishek <waghreabhishek@gmail.com>
2026-05-04 16:08:59 +0530

    helmet seo

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,22 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
+import { HelmetProvider } from "react-helmet-async";
 
 import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
 
-      <Toaster position="bottom-center"
-        reverseOrder={false} />
-      <App />
+    <HelmetProvider>
+      <BrowserRouter>
 
-    </BrowserRouter>
+        <Toaster position="bottom-center"
+          reverseOrder={false} />
+        <App />
+
+      </BrowserRouter>
+    </HelmetProvider>
+
   </StrictMode>,
 )
